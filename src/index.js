ce20032c2d65b2792eaeb89c0d56bcc8cac2be62
Drabinko Artur <artur.drabinko@gmail.com>
2018-03-05 01:16:11 +0300

    Solution with friend help

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
+const createEnumerableProperty = (propertyName) => { return propertyName };
+
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        enumerable: false,
+        value: 'value'
+    })
+    return propertyName;
+};
+const createProtoMagicObject = () => {
+    function protoMagicObject(){};
+    protoMagicObject.prototype = protoMagicObject.__proto__;
+
+    return protoMagicObject;
+};
+
 const incrementor = () => {};
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+
+
+const createIncrementer = () => {
+
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+
+const createSerializedObject = () => {
+    object = null;
+    return object;
+      
+};
+
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => { 
+
+    arr = arr.map(function (object){
+        return object.__proto__;
+    });
+
+    arr.sort( function(a, b){
+        return a- b;
+    });
+
+    return arr; 
+
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
@@ -24,3 +59,12 @@ exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
 exports.sortByProto = sortByProto;
+
+
+/*used links
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Enumerability_and_ownership_of_properties
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
+http://interosite.ru/articles/chto-takoe-prototipnoe-nasledovanie-prototipy-obektov-v-javascript
+
+*/
